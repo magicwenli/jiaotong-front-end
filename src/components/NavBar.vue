71f3030f346dc5ab663209adb01a0cd9b5f72a8e
magicwenli <yxra3603@outlook.com>
2021-07-06 10:54:48 +0800

    initial

---
+++ src/components/NavBar.vue
@@ -0,0 +1,37 @@
+<!--
+ * @Author       : magicwenli
+ * @Date         : 2021-07-06 10:28:20
+ * @LastEditTime : 2021-07-06 10:52:49
+ * @Description  : 
+ * @FilePath     : /front-end/src/components/NavBar.vue
+-->
+
+<template>
+<link href="https://cdn.bootcdn.net/ajax/libs/font-awesome/5.15.3/css/all.min.css" rel="stylesheet">
+  <div class="px-8 pt-2 mb-4 bg-white shadow-md">
+    <nav class="flex flex-row justify-center -mb-px text-xs text-gray-500 font-bold">
+      <a href="" class="mr-8 py-3 no-underline border-b-2 border-teal-500 tracking-wide text-teal-500">
+        <i class="fa fa-home"></i>
+        <span>首页</span>
+      </a>
+      <a href="" class="mr-8 py-3 no-underline border-b-2 border-transparent tracking-wide">
+        <i class="fa fa-bars"></i>
+        <span>分类</span>
+      </a>
+      <a href="" class="mr-8 py-3 no-underline border-b-2 border-transparent tracking-wide">
+        <i class="fa fa-user"></i>
+        <span>会员</span>
+      </a>
+    </nav>
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
