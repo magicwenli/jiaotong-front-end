52db9b64513d1b910dd3b5f0e2fca04aa6d5aa2b
magicwenli <yxra3603@outlook.com>
2021-07-12 10:19:53 +0800

    fix Post.vue

--- src/views/Post.vue
+++ src/views/Post.vue
@@ -1,7 +1,7 @@
 <!--
  * @Author       : magicwenli
  * @Date         : 2021-07-09 09:30:22
- * @LastEditTime : 2021-07-12 09:53:41
+ * @LastEditTime : 2021-07-12 10:19:21
  * @Description  : 
  * @FilePath     : /front-end/src/views/Post.vue
 -->
@@ -10,11 +10,11 @@
   <div class="flex flex-col mx-4">
     <el-form>
       <div class="flex flex-row mt-4">
-        <el-button class=""><i class="el-icon-arrow-left"></i>返回</el-button>
+        <router-link class="text-gray-900 text-base my-auto" to="/"><i class="el-icon-arrow-left"></i>返回</router-link>
         <div class="flex-grow my-auto">
-          <h2 class="text-gray-900 text-lg">发布动态</h2>
+          <h2 class="text-gray-900 text-xl">发布动态</h2>
         </div>
-        <el-button><i class="el-icon-check"></i>发布</el-button>
+        <router-link class="text-gray-900 text-base my-auto" to="/"><i class="el-icon-check"></i>发布</router-link>
       </div>
       <div class="mt-4">
         <el-input
@@ -26,13 +26,31 @@
         </el-input>
       </div>
 
-      <div class="flex flex-col mt-4">
-        <div class="mr-auto">
-          Preview:
+      <div class="mt-4">
+        <el-upload
+          class=""
+          action="#"
+          :on-preview="handlePreview"
+          :on-remove="handleRemove"
+          :on-change="handleChange"
+          :file-list="fileList"
+          list-type="picture"
+          with-credentials
+        >
+          <el-button class="" size="small" type="primary">点击上传</el-button>
+          <template #tip>
+            <div class="el-upload__tip">
+              只能上传 jpg/png 文件，且不超过 500kb
+            </div>
+          </template>
+        </el-upload>
+      </div>
+      <div class="flex flex-col my-4">
+        <div class="mr-auto">Preview:</div>
+        <div class="rounded-md bg-gray-50 block text-left">
+          <VueShowdown :markdown="textarea2"></VueShowdown>
         </div>
-        <div class="rounded-md bg-gray-50  block text-left">
-        <VueShowdown :markdown="textarea2" ></VueShowdown>
-      </div></div>
+      </div>
     </el-form>
   </div>
 </template>
@@ -42,13 +60,23 @@ export default {
   data() {
     return {
       textarea2: "",
+      fileList: [],
     };
   },
   methods: {
-    md2Html(text){
+    md2Html(text) {
       return Converter.makeHtml(text);
-    }
+    },
+    handleRemove(file, fileList) {
+      console.log(file, fileList);
+    },
+    handlePreview(file) {
+      console.log(file);
+    },
+    handleChange(file, fileList) {
+        this.fileList = fileList.slice(-3);
+      }
   },
-  components:[]
+  components: [],
 };
 </script>
