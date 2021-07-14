75f59db1c2274797b37b8248a6df450723cdc0f6
magicwenli <yxra3603@outlook.com>
2021-07-14 09:51:49 +0800

    图片放大；无限滚动

--- src/components/Contents.vue
+++ src/components/Contents.vue
@@ -1,7 +1,7 @@
 <!--
  * @Author       : magicwenli
  * @Date         : 2021-07-06 14:29:10
- * @LastEditTime : 2021-07-13 11:14:51
+ * @LastEditTime : 2021-07-14 09:30:35
  * @Description  : 
  * @FilePath     : /front-end/src/components/Contents.vue
 -->
@@ -9,7 +9,15 @@
 <template>
   <div class="bg-gray-200">
     <div class="flex-col py-1 -my-1 w-full mx-auto md:max-w-3/4 lg:max-w-1/2">
-      <SinglePost v-for="post in posts" :key="post.pid" v-bind="post" />
+      <ul
+        class="infinite-list"
+        v-infinite-scroll="load"
+        infinite-scroll-disabled="disabled"
+      >
+        <SinglePost v-for="post in posts" :key="post.pid" v-bind="post" />
+      </ul>
+      <p v-if="loading">加载中...</p>
+      <p v-if="noMore">没有更多了</p>
     </div>
   </div>
 </template>
@@ -25,16 +33,39 @@ export default {
   data() {
     return {
       posts: null,
+      count: 10,
+      loading: false,
     };
+  },
+  computed: {
+    noMore() {
+      return this.count >= 20;
+    },
+    disabled() {
+      return this.loading || this.noMore;
+    },
   },
   mounted() {
     API.get("/posts")
       .then((response) => {
-        console.log(response);
-        this.posts = response[0].posts
-        console.log(this.posts)
+        this.posts = response[0].posts;
+        console.log(this.posts);
       })
       .catch((error) => console.log(error));
+  },
+  methods: {
+    load() {
+      this.loading = true;
+      setTimeout(() => {
+        API.get("/posts")
+          .then((response) => {
+            this.posts =this.posts.concat(response[0].posts);
+            console.log(this.posts.length);
+          })
+          .catch((error) => console.log(error));
+        this.loading = false;
+      }, 500);
+    },
   },
 };
 </script>
--- src/components/SinglePost.vue
+++ src/components/SinglePost.vue
@@ -1,103 +1,106 @@
 <!--
  * @Author       : magicwenli
  * @Date         : 2021-07-06 16:35:02
- * @LastEditTime : 2021-07-14 00:13:32
+ * @LastEditTime : 2021-07-14 09:50:37
  * @Description  : 
- * @FilePath     : \jiaotong-front-end\src\components\SinglePost.vue
+ * @FilePath     : /front-end/src/components/SinglePost.vue
 -->
 
 <template>
-  <el-card
-    :body-style="{ padding: '0px' }"
-    class="bg-gray-50 shadow-md p-4 m-2"
-    v-loading="loading"
-  >
-    <div class="flex flex-row relative">
-      <!-- 头像 -->
-      <el-image
-        class="w-12 h-12 rounded-2xl flex-none"
-        :src="emailHash(muid)"
-        :alt="123"
-        lazy
-      />
-      <div class="flex-row px-4 text-left text-md">
-        <div>
-          <span class="font-bold text-gray-900 font-sans"
-            ><span class="text-purple-500">#&ensp;</span>{{ pid }}</span
-          >
-          <!-- <a href="" class="h-4">
+  <li>
+    <el-card
+      :body-style="{ padding: '0px' }"
+      class="bg-gray-50 shadow-md p-4 m-2"
+      v-loading="loading"
+    >
+      <div class="flex flex-row relative">
+        <!-- 头像 -->
+        <el-image
+          class="w-12 h-12 rounded-2xl flex-none"
+          :src="emailHash(muid)"
+          :alt="123"
+          lazy
+        />
+        <div class="flex-row px-4 text-left text-md">
+          <div>
+            <span class="font-bold text-gray-900 font-sans"
+              ><span class="text-purple-500">#&ensp;</span>{{ pid }}</span
+            >
+            <!-- <a href="" class="h-4">
               <i class="absolute right-0 far fa-thumbs-up"></i>
         </a> -->
-        </div>
-        <div>
-          <span class="font-light">{{ formatTimeData(createTime) }}</span>
+          </div>
+          <div>
+            <span class="font-light">{{ formatTimeData(createTime) }}</span>
+          </div>
         </div>
       </div>
-    </div>
-    <div class="relative overflow-hidden">
-      <!-- 正文 -->
-      <el-image
-        v-if="hasImg"
-        class="float-right ml-4 mt-2 mr-2 mb-2 rounded-lg w-4/12 shadow-md"
-        :src="img"
-        lazy
-      >
-        <template #error>
-          <div class="image-slot">
-            <i class="el-icon-picture-outline"></i>
-          </div>
-        </template>
-      </el-image>
-      <p class="text-justify pt-2 font-serif" style="text-indent: 2em">
-        {{ content }}
-      </p>
-    </div>
-    <div class="flex space-x-4 pt-4 justify-items-start rounded-xl">
-      <router-link
-        v-for="label in labels"
-        :key="label.name"
-        :to="label.url"
-        class="
-          px-2
-          border-green-900
-          bg-blue-100
-          rounded-tr-md
-          ring-2 ring-offset-2 ring-blue-400
-          text-sm
-        "
-      >
-        <span class="text-purple-500 text-base font-bold">#</span
-        >{{ label.name }}
-      </router-link>
-    </div>
-    <div class="flex pt-4 space-x-2">
-      <el-checkbox-button :label="up">
-        <i class="far fa-thumbs-up"></i>
-        <span>&ensp;123</span>
-      </el-checkbox-button>
+      <div class="relative overflow-hidden">
+        <!-- 正文 -->
+        <el-image
+          v-if="hasImg"
+          class="float-right ml-4 mt-2 mr-2 mb-2 rounded-lg w-4/12 shadow-md"
+          :src="img"
+          :preview-src-list="preImgList"
+          lazy
+        >
+          <template #error>
+            <div class="image-slot">
+              <i class="el-icon-picture-outline"></i>
+            </div>
+          </template>
+        </el-image>
+        <p class="text-justify pt-2 font-serif" style="text-indent: 2em">
+          {{ content }}
+        </p>
+      </div>
+      <div class="flex space-x-4 pt-4 justify-items-start rounded-xl">
+        <router-link
+          v-for="label in labels"
+          :key="label.name"
+          :to="label.url"
+          class="
+            px-2
+            border-green-900
+            bg-blue-100
+            rounded-tr-md
+            ring-2 ring-offset-2 ring-blue-400
+            text-sm
+          "
+        >
+          <span class="text-purple-500 text-base font-bold">#</span
+          >{{ label.name }}
+        </router-link>
+      </div>
+      <div class="flex pt-4 space-x-2">
+        <el-checkbox-button label="up">
+          <i class="far fa-thumbs-up"></i>
+          <span>&ensp;123</span>
+        </el-checkbox-button>
 
-      <button type="" class="flex-1 rounded-3xl bg-green-100">
-        <i class="far fa-thumbs-up"></i>
-        <span>&ensp;123</span>
-      </button>
-      <!-- <button class="flex-1 rounded-3xl bg-green-100">
+        <button type="" class="flex-1 rounded-3xl bg-green-100">
+          <i class="far fa-thumbs-up"></i>
+          <span>&ensp;123</span>
+        </button>
+        <!-- <button class="flex-1 rounded-3xl bg-green-100">
         <i class="far fa-thumbs-down"></i>
         <span>&ensp;456</span>
       </button> -->
-      <button
-        class="flex-1 rounded-3xl bg-green-100"
-        @click="showComments = !showComments"
-      >
-        <i class="fas fa-reply"></i>
-        <span>&ensp;789</span>
-      </button>
-    </div>
-    <div class="pt-4 overflow-hidden">
-      <transition name="comments">
-        <Comments v-if="showComments" />
-      </transition>
-    </div>
-  </el-card>
+        <button
+          class="flex-1 rounded-3xl bg-green-100"
+          @click="showComments = !showComments"
+        >
+          <i class="fas fa-reply"></i>
+          <span>&ensp;789</span>
+        </button>
+      </div>
+      <div class="pt-4 overflow-hidden">
+        <transition name="comments">
+          <Comments v-if="showComments" />
+        </transition>
+      </div>
+    </el-card>
+  </li>
 </template>
 
 <script>
@@ -131,6 +134,14 @@ export default {
   mounted() {
     this.init();
   },
+  computed:{
+    preImgList(){
+      var list = [];
+      list.push(this.img)
+      console.log(list);
+      return list
+    }
+  }
 };
 </script>
 
