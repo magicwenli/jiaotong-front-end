2d230f088ea8e51aa8811ed8491ad4d7e03a19f5
THHH0Sunshine <menghenry7@sina.com>
2021-07-14 11:24:48 +0800

    评论

--- src/components/Comments.vue
+++ src/components/Comments.vue
@@ -8,7 +8,22 @@
     </template>
     <div v-if="comments.length">
       <div v-for="(comment, k) in comments" :key="k">
-        <div>{{ comment.content }}</div>
+        <div class="title">
+          <div class="user">
+            <el-image class="avatar" :src="emailHash(comment.muid)" lazy/>
+            <div>
+              <div>{{ comment.muid == muid ? '洞主' : comment.muid }}</div>
+              <div>{{ formatTime(comment.createTime) }}</div>
+            </div>
+          </div>
+          <div>{{ comment.floor }}楼</div>
+        </div>
+        <div class="content">{{ comment.content }}</div>
+        <div class="buttons">
+          <el-button class="button" type="text"><i class="far fa-thumbs-up"></i> 赞</el-button>
+          <el-button class="button" type="text" @click="reply(comment.floor)"><i class="fas fa-reply"></i> 回复</el-button>
+          <el-button class="button" type="text"><i class="far fa-thumbs-down"></i> 踩</el-button>
+        </div>
         <el-divider/>
       </div>
     </div>
@@ -18,25 +33,29 @@
     </div>
     <el-pagination layout="prev, pager, next" :page-size="pageSize" :total="total" :current-page="page" @current-change="turn"/>
     <div class="submit">
-      <el-input placeholder="请输入评论内容..."/>
-      <el-button class="submit-button" type="primary">发布</el-button>
+      <el-input ref="input" size="medium" placeholder="请输入评论内容..." v-model="replying"/>
+      <el-button class="submit-button" size="medium" type="primary">发布</el-button>
     </div>
   </el-card>
 </template>
 
 <script>
 import { getCommentsOfPost } from '../utils/api/comments.js'
+import formatTime from '../utils/TimeFormater.vue'
+import md5 from 'js-md5'
 
 export default {
   props: {
-    pid: Number
+    pid: Number,
+    muid: Number
   },
   data() {
     return {
       comments: [],
       pageSize: 10,
       total: 0,
-      page: 1
+      page: 1,
+      replying: ''
     }
   },
   methods: {
@@ -44,6 +63,13 @@ export default {
       try {
         const data = await getCommentsOfPost(this.pid, 1, 10)
         this.comments = data[0].comments
+        this.comments.push({
+          content: 'Hello world!',
+          createTime: '2021-01-01 00:00:00',
+          floor: -1,
+          info: [{ likes: 100, dislikes: 100 }],
+          muid: this.muid
+        })
         this.total = data[0].comments.length
       } catch (e) {
         this.$message.error('获取评论列表失败：' + e)
@@ -52,6 +78,17 @@ export default {
     async turn(page) {
       this.page = page
       await this.loadComments()
+    },
+    formatTime(time) {
+      return formatTime(new Date(time).getTime() / 1000)
+    },
+    emailHash(email) {
+      return "https://gravatar.loli.net/avatar/" + md5(email) + "?d=monsterid";
+    },
+    reply(floor) {
+      this.replying = '回复' + floor + '楼：'
+      this.$el.scrollIntoView(false)
+      this.$refs.input.focus()
     }
   },
   async mounted() {
@@ -70,6 +107,36 @@ export default {
 .button {
   padding: 0;
   min-height: auto;
+}
+
+.title {
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+}
+
+.user {
+  display: flex;
+  align-items: center;
+  text-align: left;
+  font-size: 14px;
+}
+
+.avatar {
+  margin-right: 15px;
+  width: 40px;
+  height: 40px;
+  border-radius: 10px;
+}
+
+.content {
+  margin-top: 10px;
+  text-align: left;
+}
+
+.buttons {
+  margin-top: 10px;
+  display: flex;
 }
 
 .submit {
--- src/components/SinglePost.vue
+++ src/components/SinglePost.vue
@@ -93,7 +93,7 @@
       </div>
       <div class="pt-4 overflow-hidden">
         <transition name="comments">
-          <Comments v-if="showComments" />
+          <Comments v-if="showComments" :pid="pid" :muid="muid"/>
         </transition>
       </div>
     </el-card>
