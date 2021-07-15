9d8649d0dc8ed41f8aef22931fbfd9debe088e21
magicwenli <yxra3603@outlook.com>
2021-07-15 16:57:39 +0800

    add color template

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,7 +3,14 @@ module.exports = {
   darkMode: false, // or 'media' or 'class'
   theme: {
     extend: {
-      boxShadow:['active']
+      boxShadow: ['active'],
+      colors: {
+        'color-1': '#0f0b26',
+        'color-2': '#522421',
+        'color-3': '#8c5a2e',
+        'color-4': '#bf8641',
+        'color-5': '#b3b372',
+      },
     },
     maxWidth: {
       '0': '0',
@@ -11,10 +18,10 @@ module.exports = {
       '1/2': '50%',
       '3/4': '75%',
       'full': '100%',
-     },
-     fontFamily:{
-        'sans':['webfont','BlinkMacSystemFont',"Segoe UI",'Roboto','Oxygen','Ubuntu','Cantarell',"Open Sans"]
-     }
+    },
+    fontFamily: {
+      'sans': ['webfont', 'BlinkMacSystemFont', "Segoe UI", 'Roboto', 'Oxygen', 'Ubuntu', 'Cantarell', "Open Sans"]
+    }
     //  -apple-system,,"Helvetica Neue","Helvetica",PingFang SC,PingFang TC,Hiragino Sans GB,STHeiti,Microsoft YaHei,Microsoft JhengHei,"WenQuanYi Micro Hei",sans-serif
   },
   variants: {
